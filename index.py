6b0746b750fadb35158a2f1c66b7fb944841f3c8
kanda WANG <kanda.wang@ingka.ikea.com>
2026-02-24 17:00:12 +0800

    fix: add logging for heimdallr_URL, heimdallr_TOKEN, and heimdallr_GROUP_KEY

--- index.py
+++ index.py
@@ -12,14 +12,11 @@ import os
 def handler(event, context):
     logger = logging.getLogger()
     heimdallr_URL = os.getenv('heimdallr_URL')
-    logger.info(f'heimdallr_URL = {heimdallr_URL}')
     heimdallr_TOKEN = os.getenv('heimdallr_TOKEN')   
     heimdallr_GROUP_KEY = os.getenv('heimdallr_GROUP_KEY')
-
+ 
     evt = json.loads(event)
-    logger.info(f'evt = {evt}')
     body =evt.get("body")
-    logger.info(f'body = {body}')
     if body is None:
         logger.error('body is None')
         return {
@@ -29,24 +26,16 @@ def handler(event, context):
     else:
         body = json.loads(body)
         id = body.get("id")
-        logger.info(f'id = {id}')
         url = body.get("url")
-        logger.info(f'url = {url}')
         value = body.get("value")
-        logger.info(f'value = {value}')
         html = body.get("html")
-        logger.info(f'html = {html}')
         link = body.get("link")
-        logger.info(f'link = {link}')
         data = body.get("data")
-        logger.info(f'data = {data}')
 
     title = f'监测任务ID: {id}, 状态：{data}'
-    logger.info(f'title = {title}')
     msg_type = 'markdown'
     content = f'>**监控值** <font color=\"info\">{value}</font>  \n> [详情链接]({link})  *** {html}'
     markdown = {"content": content}
-    logger.info(f'markdown = {markdown}')
 
 
     payload = {
@@ -55,11 +44,13 @@ def handler(event, context):
         "msg_type": msg_type,
         "markdown": markdown
     }
+    logger.info(f'payload = {payload}')
 
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {heimdallr_TOKEN}"
     }
+    logger.info(f'Payload to Heimdallr: {payload}')
 
     response = requests.post(heimdallr_URL, json=payload, headers=headers, timeout=120)
     logger.info(f'Heimdallr response status code: {response.status_code}')
